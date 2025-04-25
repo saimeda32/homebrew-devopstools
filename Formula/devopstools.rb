e2addd538d78a9aed927d92179de0ac07374be7a
saimeda32 <80650978+saimeda32@users.noreply.github.com>
2025-04-25 16:45:07 -0500

    Fix: updated SHA256 for main branch tarball

--- Formula/devopstools.rb
+++ Formula/devopstools.rb
@@ -2,7 +2,7 @@ class Devopstools < Formula
   desc "Bundle installer for DevOps tools"
   homepage "https://github.com/saimeda32/homebrew-devopstools"
   url "https://github.com/saimeda32/homebrew-devopstools/archive/refs/heads/main.tar.gz"
-  sha256 "0" * 64 # use a dummy value to bypass real hash check (for now)
+  sha256 "4b8aa0547c0bafafca86046e58db1675b86e4d24b7b47eced589b26f0637d68e"
   version "1.0.0"
 
   def install
